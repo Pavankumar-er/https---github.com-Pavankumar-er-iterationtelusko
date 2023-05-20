751c3f64087a84ebfc603f25717b4aa00015d1fe
Pavankumar-er <pavankumare865>
2023-05-20 16:46:17 +0530

    first commit

--- hy.java
+++ hy.java
@@ -1,10 +1,20 @@
 public class hy
- {
-    public static void main (String a[])
-
+{
+    public static void main(String[]args)
     {
-    char c='k';
-    c++;
-    System.out.println(c);
+        for(int i=0;i<=4;i++)
+        {
+            for (int j=1;j<4-i;j++)
+            {
+                System.out.print(" ");
+            }
+            int number=1;
+            for(int k=0;k<=i;k++)
+            {
+                System.out.print(number+" ");
+                number=number*(i-k)/(k+1);
+            }
+            System.out.println();
+        }
     }
 }
